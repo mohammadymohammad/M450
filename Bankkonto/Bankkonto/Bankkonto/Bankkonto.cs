f51b693b1c3d7f0dd1c79730c220b9d384831a8c
mohammadymohammad <mohammad.mohammady@edu.gbssg.ch>
2026-08-30 23:17:14 +0200

    Bankkonto Klasserstellung

---
+++ Bankkonto/Bankkonto/Bankkonto/Bankkonto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bankkonto;
+
+public class Bankkonto
+{
+    public string KontoNummer { get; private set; } = Guid.NewGuid().ToString();
+    public decimal Guthaben { get; private set; }
+    public static decimal AktivZins { get; set; }
+    public static decimal PassivZins { get; set; }
+
+
+    public void Einzahlen(decimal betrag)
+    {
+        if (betrag <= 0)
+        {
+            throw new ArgumentException("Der Betrag muss grösser als 0 sein.");
+        }
+
+        Guthaben += betrag;
+    }
+
+    public void Beziehen(decimal betrag)
+    {
+        if (betrag <= 0)
+        {
+            throw new ArgumentException("Der Betrag muss grösser als 0 sein.");
+        }
+
+        if (betrag > Guthaben)
+        {
+            throw new InvalidOperationException("Nicht genügend Guthaben auf dem Konto.");
+        }
+
+        Guthaben -= betrag;
+    }
+
+    public void Transferieren(Bankkonto konto, decimal betrag)
+    {
+        if (betrag <= 0)
+        {
+            throw new ArgumentException("Der Betrag muss grösser als 0 sein.");
+        }
+
+        if (betrag > Guthaben)
+        {
+            throw new InvalidOperationException("Nicht genügend Guthaben auf dem Konto.");
+        }
+
+        Guthaben -= betrag;
+        konto.Einzahlen(betrag);
+    }
+
+    public void Zinsgutschreibung(int anzahlTage)
+    {
+        if (Guthaben > 0)
+        {
+            Guthaben += Guthaben * AktivZins * anzahlTage / 365;
+        }
+        else if (Guthaben < 0)
+        {
+            Guthaben += Guthaben * PassivZins * anzahlTage / 365;
+        }
+    }
+
+    public void Kontoabschliessen()
+    {
+        Guthaben = 0;
+    }
+
+}
---
+++ Bankkonto/Bankkonto/BankkontoTest/BankkontoTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankkontoTest;
+
+public class BankkontoTest
+{
+    [Fact]
+    public void TestEinzahlen()
+    {
+        // Test code for Einzahlen method
+    }
+}
