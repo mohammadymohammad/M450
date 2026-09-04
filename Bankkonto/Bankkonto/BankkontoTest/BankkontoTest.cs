432a0223c0903bea7d5147a3a3c7e5d0a076db17
mohammadymohammad <mohammad.mohammady@edu.gbssg.ch>
2026-09-04 15:53:29 +0200

    Zwei test methode

--- Bankkonto/Bankkonto/BankkontoTest/BankkontoTest.cs
+++ Bankkonto/Bankkonto/BankkontoTest/BankkontoTest.cs
@@ -7,8 +7,25 @@ namespace BankkontoTest;
 public class BankkontoTest
 {
     [Fact]
-    public void TestEinzahlen()
+    void Deposit_0_or_Negative_Amount_Throws_ArgumentException()
     {
-        // Test code for Einzahlen method
+        // Arrange
+        var konto = new Bankkonto.Bankkonto();
+        decimal depositAmount = 0m;
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => konto.Einzahlen(depositAmount));
+    }
+
+    [Fact]
+    void Deposit_100_Amount_IncreasesBalanceBy100()
+    {
+        // Arrange
+        var konto = new Bankkonto.Bankkonto();
+        decimal initialBalance = konto.Guthaben;
+        decimal depositAmount = 100m;
+        // Act
+        konto.Einzahlen(depositAmount);
+        // Assert
+        Assert.Equal(initialBalance + depositAmount, konto.Guthaben);
     }
 }
